150d106bac7384a5b697b85dacc51348dd890a48
rizqitsani <rizqitsani@gmail.com>
2024-01-05 23:19:00 +0700

    style: format code

--- packages/ui/lib/classnames.ts
+++ packages/ui/lib/classnames.ts
@@ -3,6 +3,6 @@ import clsx from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 /** Merge classes with tailwind-merge with clsx full feature */
-export default function cn(...classes: ClassValue[]):string {
+export default function cn(...classes: ClassValue[]): string {
   return twMerge(clsx(...classes));
 }
