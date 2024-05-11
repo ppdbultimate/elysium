27f535c0f0bbd12010b9c567f945899ea6324ab2
rizqitsani <rizqitsani@gmail.com>
2024-05-11 17:14:22 +0700

    feat: export type File for forms

--- packages/ui/src/types/dropzone.ts
+++ packages/ui/src/types/dropzone.ts
@@ -1,3 +1,6 @@
 import type { FileWithPath } from 'react-dropzone';
 
 export type FileWithPreview = FileWithPath & { preview: string };
+
+// Used for react hook form field type
+export type File = string | { preview: string; name: string; type: string }[];
